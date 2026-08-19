ff03cc6dd9bcbfc010092e8f826416b9f32d2261
CxzDeveloper <krisnaokeh@gmail.com>
2026-08-19 15:46:47 +0700

    Add files via upload

---
+++ app/admin/login/page.tsx
@@ -0,0 +1,3 @@
+import SiteShell from "@/components/SiteShell";
+import LoginForm from "@/components/LoginForm";
+export default function Login(){ return <SiteShell><main className="page fade-in"><LoginForm/></main></SiteShell> }
