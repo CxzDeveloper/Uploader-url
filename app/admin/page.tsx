7951260158478389fa27bcc6561ea4a0eb4a57fe
CxzDeveloper <krisnaokeh@gmail.com>
2026-08-19 15:46:09 +0700

    Add files via upload

---
+++ app/admin/page.tsx
@@ -0,0 +1,3 @@
+import SiteShell from "@/components/SiteShell";
+import AdminPanel from "@/components/AdminPanel";
+export default function Admin(){ return <SiteShell><main className="page fade-in"><AdminPanel/></main></SiteShell> }
